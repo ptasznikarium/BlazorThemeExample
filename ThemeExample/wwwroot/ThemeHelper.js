87d6e9f376b148c49445b1ca8f5120660944afac
Kanda <1194703+Kanda@users.noreply.github.com>
2024-03-17 00:13:09 +0100

    Initial commit

---
+++ ThemeExample/wwwroot/ThemeHelper.js
@@ -0,0 +1,13 @@
+﻿
+    function switchTheme(value) {
+        document.documentElement.setAttribute("data-bs-theme", value)
+        localStorage.setItem("data-bs-theme", value);
+    };
+
+    function getTheme() {
+        return localStorage.getItem("data-bs-theme") ?? "light";
+    };
+
+    (function (){
+        document.documentElement.setAttribute("data-bs-theme", localStorage.getItem("data-bs-theme") ?? "light")
+    }());
